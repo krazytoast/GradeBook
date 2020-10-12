eab56a0b4064fbc3b3b3ce99d0da592d3079b4e2
Zachery Berkey <31834796+krazytoast@users.noreply.github.com>
2020-10-11 17:45:11 -0700

    C# GradeBook
    Created classes and reduced code from Main

--- src/GradeBook/book.cs
+++ src/GradeBook/book.cs
@@ -1,12 +1,14 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook
 {
     class Book
     {
-        public Book()
+        public Book(string name)
         {
             grades = new List<double>();
+            this.name = name;
         }
 
         public void AddGrade(double grade)
@@ -14,6 +16,24 @@ namespace GradeBook
             grades.Add(grade);
         }
 
-        List<double> grades;
+        public void ShowStatistics()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            foreach(var number in grades)
+            {
+                lowGrade = Math.Min(number, lowGrade);
+                highGrade = Math.Max(number, highGrade);
+                result += number;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"The lowest grade is {lowGrade}");
+            Console.WriteLine($"The highest grade is {highGrade}");
+            Console.WriteLine($"The average grade is {result:N1}");
+        }
+
+        private List<double> grades;
+        private string name;
     }
 }
