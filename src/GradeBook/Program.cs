1a878fc2065a8163bc0288fa2b08be9275e62b96
Zachery Berkey <31834796+krazytoast@users.noreply.github.com>
2020-10-11 17:00:54 -0700

    Working with classes and objects
    Creating a class, adding state behavior, defining methods...

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,6 +7,9 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            var book = new Book();
+            book.AddGrade(89.1);
+
             var grades = new List<double>() {12.7, 10.3, 6.11, 4.1};
             grades.Add(56.1);
              
@@ -16,17 +19,7 @@ namespace GradeBook
                 result += number;
             }
             result /= grades.Count;
-            Console.WriteLine($"The average grade is {result:N2}");
-
-
-            if(args.Length > 0)
-            {
-            Console.WriteLine($"Hello, {args[0]}!");
-            }
-            else
-            {
-            Console.WriteLine("Hello!");
-            }
+            Console.WriteLine($"The average grade is {result:N1}");
         }
     }
 }
