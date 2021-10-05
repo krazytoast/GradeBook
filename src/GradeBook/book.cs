989885ffa4c0fecb15048418d3bfacf6f77c9b13
Zachery Berkey <31834796+krazytoast@users.noreply.github.com>
2021-10-04 18:36:18 -0700

    switch cases for Grades to Letters
    created two different switch cases to compare for providing a Letter grade with corresponding grade number.

--- src/GradeBook/book.cs
+++ src/GradeBook/book.cs
@@ -11,6 +11,29 @@ namespace GradeBook
             Name = name;
         }
 
+        // public void AddLetterGrade(char letter)
+        // {
+        //     switch(letter)
+        //     {
+        //         case 'A':
+        //             AddGrade(90);
+        //             break;
+        //         case 'B':
+        //             AddGrade(80);
+        //             break;
+        //         case 'C':
+        //             AddGrade(70);
+        //             break;
+        //         case 'D':
+        //             AddGrade(60);
+        //             break;
+
+        //         default:
+        //             AddGrade(0);
+        //             break;
+        //     }
+        // }
+
         public void AddGrade(double grade)
         {
             if(grade <= 100 && grade >= 0)
@@ -37,7 +60,32 @@ namespace GradeBook
                 result.High = Math.Max(grade, result.High);
                 result.Average += grade;
             }
+
             result.Average /= grades.Count;
+
+            switch(result.Average)
+            {
+                case var d when d >= 90.0:
+                    result.Letter = 'A';
+                    break;
+
+                case var d when d >= 80.0:
+                    result.Letter = 'B';
+                    break;
+
+                case var d when d >= 70.0:
+                    result.Letter = 'C';
+                    break;
+
+                case var d when d >= 60.0:
+                    result.Letter = 'D';
+                    break;
+
+                default:
+                    result.Letter = 'F';
+                    break;
+
+            }
 
             return result;
         }
