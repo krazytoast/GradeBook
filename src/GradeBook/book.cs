59683825cd55e70a94f6b30833098ce738a5f09a
Zachery Berkey <31834796+krazytoast@users.noreply.github.com>
2021-10-05 21:45:04 -0700

    delegates
    creating a unit test for a delegate

--- src/GradeBook/book.cs
+++ src/GradeBook/book.cs
@@ -93,19 +93,10 @@ namespace GradeBook
 
         public string Name
         {
-            get
-            {
-                return name;
-            }
-            set
-            {
-                if(!String.IsNullOrEmpty(value))
-                {
-                    name = value;
-                }
-            }
-        }
+            get;
+            private set;
+        {
 
-        private string name;
+        public const string CATEGORY = "Science";
     }
 }
